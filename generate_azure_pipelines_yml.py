86eaad12f4e6a5e0146ddf0e542c7bf69ffb1e43
Hiroshi Miura <miurahr@linux.com>
2019-05-29 08:01:04 +0900

    azure: fix dir for mingw tests
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- generate_azure_pipelines_yml.py
+++ generate_azure_pipelines_yml.py
@@ -80,9 +80,9 @@ windows_build_jobs.extend(
     [
         BuildJob('5.13.0', 'windows', 'desktop', 'win64_msvc2017_64', 'msvc2017_64'),
         BuildJob('5.13.0', 'windows', 'desktop', 'win64_msvc2015_64', 'msvc2015_64'),
-        BuildJob('5.13.0', 'windows', 'desktop', 'win64_mingw73', 'mingw73'),
+        BuildJob('5.13.0', 'windows', 'desktop', 'win64_mingw73', 'mingw73_64'),
         BuildJob('5.13.0', 'windows', 'desktop', 'win32_msvc2017', 'msvc2017'),
-        BuildJob('5.13.0', 'windows', 'desktop', 'win32_mingw73', 'mingw73'),
+        BuildJob('5.13.0', 'windows', 'desktop', 'win32_mingw73', 'mingw73_32'),
     ]
 )
 
