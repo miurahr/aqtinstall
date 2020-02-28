8acf4f426b3fddde64507155dbf522993d43b678
Hiroshi Miura <miurahr@linux.com>
2020-02-28 11:43:14 +0900

    Update test
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -5,8 +5,11 @@ import aqt
 
 @pytest.mark.remote_data
 def test_cli_unknown_version(capsys):
-    expected = ""
+    expected = ["aqt - WARNING - Specified Qt version is unknown: 5.12.",
+                "aqt - ERROR - Download error when access to https://download.qt.io/online/qtsdkrepository/mac_x64/desktop/qt5_512/Updates.xml Server response code: 404, reason code: Not Found"
+               ]
     cli = aqt.cli.Cli()
     cli.run(["install", "5.12", "mac", "desktop"])
     out, err = capsys.readouterr()
-    assert out == expected
+    for i, line in enumerate(out):
+        assert line.endswith(expected[i])
