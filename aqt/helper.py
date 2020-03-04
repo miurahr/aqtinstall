beb755219a092ec319c8a0672cf865bdb3296e29
Hiroshi Miura <miurahr@linux.com>
2020-03-04 16:30:16 +0900

    Check redirect location before get metalink data
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- aqt/helper.py
+++ aqt/helper.py
@@ -5,33 +5,38 @@ import requests
 from aqt.settings import Settings
 
 
-def altlink(url, priority=None):
+def altlink(url, alt, priority=None):
     '''Download .meta4 metalink version4 xml file and parse it.'''
-
-    mirrors = {}
-    url = url
     settings = Settings()
-    blacklist = settings.blacklist
+    black = False
+    for site in settings.blacklist:
+        if alt.startswith(site):
+            black = True
+    if not black:
+        return alt
+    # specified mirror is black, try select another.
+    mirrors = {}
     try:
         m = requests.get(url + '.meta4')
     except requests.exceptions.ConnectionError:
         return
     else:
-        mirror_xml = ElementTree.fromstring(m.text)
-        for f in mirror_xml.iter("{urn:ietf:params:xml:ns:metalink}file"):
-            for u in f.iter("{urn:ietf:params:xml:ns:metalink}url"):
-                pri = u.attrib['priority']
-                mirrors[pri] = u.text
+        if m.headers['content-type'] == 'application/metalink4+xml':
+            mirror_xml = ElementTree.fromstring(m.text)
+            for f in mirror_xml.iter("{urn:ietf:params:xml:ns:metalink}file"):
+                for u in f.iter("{urn:ietf:params:xml:ns:metalink}url"):
+                    pri = u.attrib['priority']
+                    mirrors[pri] = u.text
+        if len(mirrors) == 0:
+            # there is no valuable data.
+            return alt
 
-    if len(mirrors) == 0:
-        # no alternative
-        return url
     if priority is None:
-        if blacklist is not None:
+        if settings.blacklist is not None:
             for ind in range(len(mirrors)):
                 mirror = mirrors[str(ind + 1)]
                 black = False
-                for b in blacklist:
+                for b in settings.blacklist:
                     if mirror.startswith(b):
                         black = True
                         continue
