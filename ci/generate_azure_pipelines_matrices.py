bd76a3a39f9e5d78075b8aceb82bd689a46666d4
Hiroshi Miura <miurahr@linux.com>
2020-04-15 13:05:04 +0900

    ci: add test case: 5.14.2 on mac
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- ci/generate_azure_pipelines_matrices.py
+++ ci/generate_azure_pipelines_matrices.py
@@ -119,8 +119,11 @@ linux_build_jobs.extend(
         BuildJob('5.14.0', 'linux', 'desktop', 'gcc_64', 'gcc_64', module='all')
     ]
 )
-mac_build_jobs.append(
-    BuildJob('5.13.2', 'mac', 'desktop', 'clang_64', 'clang_64', module='qcharts qtnetworkauth')
+mac_build_jobs.extend(
+    [
+    BuildJob('5.13.2', 'mac', 'desktop', 'clang_64', 'clang_64', module='qcharts qtnetworkauth'),
+    BuildJob('5.14.2', 'mac', 'desktop', 'clang_64', 'clang_64', module='qcharts qtnetworkauth')
+    ]
 )
 windows_build_jobs.append(
     BuildJob('5.13.2', 'windows', 'desktop', 'win64_msvc2017_64', 'msvc2017_64', module='qcharts qtnetworkauth')
