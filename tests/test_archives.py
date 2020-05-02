bd681682da015da3b92ac3fe45b357ae01be5002
Hiroshi Miura <miurahr@linux.com>
2020-05-02 09:08:56 +0900

    unitest: case for msvc2017_64
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_archives.py
+++ tests/test_archives.py
@@ -8,6 +8,7 @@ from aqt.archives import QtArchives
 
 @pytest.mark.parametrize("os_name,version,target,datafile", [
     ('windows', '5.14.0', 'win64_mingw73', 'windows-5140-update.xml'),
+    ('windows', '5.15.0', 'win64_msvc2017_64', 'windows-5150-update.xml'),
     ('windows', '5.15.0', 'win64_mingw73', 'windows-5150-update.xml'),
 ])
 def test_parse_update_xml(monkeypatch, os_name, version, target, datafile):
