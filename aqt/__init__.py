1f02d7ae5897f7329492d2b9a75c838b528f2e09
Hiroshi Miura <miurahr@linux.com>
2021-06-11 08:57:15 +0900

    Manifest: ignore version.py and ci files
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- aqt/__init__.py
+++ aqt/__init__.py
@@ -20,8 +20,8 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from aqt.version import __version__
 from aqt.installer import Cli
+from aqt.version import __version__
 
 __all__ = ["__version__"]
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     use_scm_version={
         "write_to": "aqt/version.py",
-        "write_to_template": '__version__ = "{version}"',
+        "write_to_template": '__version__ = "{version}"\n',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
         "local_scheme": "no-local-version",
     }
