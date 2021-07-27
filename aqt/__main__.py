ac38cc7b3967957c959fdd1876fa38f3c397d1fc
Hiroshi Miura <miurahr@linux.com>
2021-07-28 06:53:45 +0900

    Exit when run on python 2 with warning
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- aqt/__main__.py
+++ aqt/__main__.py
@@ -19,13 +19,12 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import platform
 import sys
 
 from . import main
 
 if __name__ == "__main__":
-    if platform.python_version_tuple() < (3, 6, 0):
-        print("aqtinstall requires python 3.6 or later.")
+    if sys.version_info[0] == "2":
+        print("aqtinstall requires python 3!")
         sys.exit(1)
     sys.exit(main())
