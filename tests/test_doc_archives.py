a2e6822e84f5746433689dc7546739bda16eaa74
David Dalcino <ddalcino@gmail.com>
2021-08-23 10:06:23 -0700

    Test SrcDocExamplesArchives properly
    The existing tests in test_doc_archives.py don't actually test the
    SrcDocExamplesArchives object; for some reason, they just test
    QtArchives. The old tests make it hard to change the behavior of
    QtArchives without causing these tests to fail.
    
    This change alters these tests so that they use SrcDocExamplesArchives,
    which is otherwise untested. It also changes the name of the "arch"
    variable to "flavor", which is more accurate for the
    SrcDocExamplesArchives class. It also adds tests for the untested
    flavors, "src" and "examples".

--- tests/test_doc_archives.py
+++ tests/test_doc_archives.py
@@ -2,7 +2,7 @@ import os
 
 import pytest
 
-from aqt.archives import QtArchives
+from aqt.archives import QtArchives, SrcDocExamplesArchives
 from aqt.helper import Settings
 
 
@@ -12,30 +12,31 @@ def setup():
 
 
 @pytest.mark.parametrize(
-    "os_name,version,target,datafile",
-    [("windows", "5.15.2", "doc", "windows-5152-src-doc-example-update.xml")],
+    "os_name, version, flavor, datafile",
+    (
+        ("windows", "5.15.2", "doc", "windows-5152-src-doc-example-update.xml"),
+        ("windows", "5.15.2", "src", "windows-5152-src-doc-example-update.xml"),
+        ("windows", "5.15.2", "examples", "windows-5152-src-doc-example-update.xml"),
+    ),
 )
-def test_parse_update_xml(monkeypatch, os_name, version, target, datafile):
+def test_parse_update_xml(monkeypatch, os_name, version, flavor, datafile):
     def _mock(self, url):
         with open(os.path.join(os.path.dirname(__file__), "data", datafile), "r") as f:
             self.update_xml_text = f.read()
 
     monkeypatch.setattr(QtArchives, "_download_update_xml", _mock)
 
-    qt_archives = QtArchives(os_name, "desktop", version, target, Settings.baseurl)
+    qt_archives = SrcDocExamplesArchives(flavor, os_name, "desktop", version, Settings.baseurl)
     assert qt_archives.archives is not None
 
     # Get packages with all extra modules
-    qt_archives_all_modules = QtArchives(
+    qt_archives_all_modules = SrcDocExamplesArchives(
+        flavor,
         os_name,
         "desktop",
         version,
-        target,
         Settings.baseurl,
-        None,
-        ["all"],
-        None,
-        True,
+        all_extra=True,
     )
     assert qt_archives_all_modules.archives is not None
 
@@ -45,7 +46,7 @@ def test_parse_update_xml(monkeypatch, os_name, version, target, datafile):
 
     # Check the difference list contains only extra modules urls for target specified
     list_diff = [item for item in url_all_modules_list if item not in url_list]
-    unwanted_targets = [item for item in list_diff if target not in item]
+    unwanted_targets = [item for item in list_diff if flavor not in item]
 
     # Assert if list_diff contains urls without target specified
     assert unwanted_targets == []
