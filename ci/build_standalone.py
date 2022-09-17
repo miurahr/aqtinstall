5306637bbf1dcfd0770b3e3cd4420f253cd3eeb6
Hiroshi Miura <miurahr@linux.com>
2022-09-17 23:17:17 +0900

    Update standalone binary build scripts
    - Introduce build_standalone.py
    - CI: update standalone build method
    - Genearte pseudo script to trick pyppyn satisfy
    - Generate setup.py to trick pyppyn satisfy
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ ci/build_standalone.py
@@ -0,0 +1,34 @@
+import argparse
+
+import gravitybee
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("arch", nargs="?")
+    args = parser.parse_args()
+
+    # generate pseudo script
+    # pip does not generate console_script any more but gravitybee expect it.
+    with open("build_venv/Scripts/aqtinstall.py", "w") as f:
+        f.write("import aqt\nif __name__ == \"__main__\":\n    aqt.main()\n")
+
+    # generate setup.py
+    # pyppyn build wheel with deprecated setup.py bdist_wheel
+    # so fake it with dummy setup.py
+    with open("setup.py", "w") as f:
+        f.write("import setuptools\nsetuptools.setup()\n")
+
+    gbargs = gravitybee.Arguments(
+        app_name="aqtinstall",
+        pkg_name="aqt",
+        script_path="build_venv/Scripts/aqtinstall.py",
+        src_dir=".",
+        pkg_dir=".",
+        clean=False,
+        with_latest=True,
+        name_format="aqt" if args.arch is None else "aqt-" + args.arch,
+    )
+
+    pg = gravitybee.PackageGenerator(gbargs)
+    pg.generate()
