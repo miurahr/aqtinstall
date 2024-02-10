301ff5173b1669e1b33f7f328b310c05a7b16ffd
David Dalcino <ddalcino@users.noreply.github.com>
2024-02-09 22:53:27 -0800

    Fix generate combinations (#757)
    * fix generate_combinations issue with Linux Qt 6.7
    
    Qt 6.7.0 changes the default arch for Linux from gcc_64 to linux_gcc_64.
    This allows the generate_combinations.py script to account for that.
    
    Also, Qt 6.7.0 is missing a few architectures that were present in
    earlier versions of Qt6. This makes sure that the script can pick those
    up.
    
    * black (v 24.1.1)
    
    New version of black formats metadata.py differently than before
    
    * Fix broken generate_combinations.py script
    
    Versions 6.5 and 6.2 must be complete versions
    
    Don't try to fetch Qt6 for target==winrt; it doesn't exist

--- ci/generate_combinations.py
+++ ci/generate_combinations.py
@@ -43,6 +43,8 @@ def iter_arches() -> Generator[dict, None, None]:
     archive_ids = list(iter_archive_ids(category="qt"))
     for archive_id in tqdm(archive_ids):
         for version in ("latest", "6.5.3", "6.2.4", "5.15.2", "5.13.2", "5.9.9"):
+            if archive_id.target == "winrt" and (version == "latest" or version.startswith("6")):
+                continue
             for arch_name in MetadataFactory(archive_id, architectures_ver=version).getList():
                 yield {
                     "os_name": archive_id.host,
