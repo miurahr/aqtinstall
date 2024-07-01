e426d1a7a10a2eea02e041fcb506b29e23917030
Hiroshi Miura <miurahr@linux.com>
2024-07-02 08:22:05 +0900

    style: wrap a long line
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- aqt/archives.py
+++ aqt/archives.py
@@ -363,7 +363,8 @@ class QtArchives:
 
     def _get_archives(self):
         if self.version >= Version("6.8.0"):
-            name = f"qt{self.version.major}_{self._version_str()}/qt{self.version.major}_{self._version_str()}{self._arch_ext()}"
+            name = (f"qt{self.version.major}_{self._version_str()}"
+                    f"/qt{self.version.major}_{self._version_str()}{self._arch_ext()}")
         else:
             name = f"qt{self.version.major}_{self._version_str()}{self._arch_ext()}"
         self._get_archives_base(name, self._target_packages())
