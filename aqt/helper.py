fecf29980eb59e2600fd7a13e92dc37bad795f8d
Alex Spataru <alex_spataru@outlook.com>
2025-04-16 23:38:59 -0500

    Update helper.py to fix download issues on Silicon Macs

--- aqt/helper.py
+++ aqt/helper.py
@@ -147,6 +147,13 @@ def get_qt_installers() -> dict[str, str]:
 
 def get_qt_installer_name() -> str:
     installer_dict = get_qt_installers()
+    
+    # If mac-arm64 is missing but mac-x64 exists, use that
+    if os_arch == "mac-arm64" and os_arch not in installer_dict:
+        fallback = "mac-x64"
+        if fallback in installer_dict:
+            return installer_dict[fallback]
+            
     return installer_dict[get_os_arch()]
 
 
